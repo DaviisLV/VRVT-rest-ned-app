570e17e03c27666d812345ae113c4a40ef8a0583
DaviisLV <abols.davis@gmail.com>
2018-11-10 16:08:13 +0200

    add new activity

--- app/src/main/java/com/abols/davis/vrvt_rest_ned_app/MainActivity.java
+++ app/src/main/java/com/abols/davis/vrvt_rest_ned_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.abols.davis.vrvt_rest_ned_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.mainButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListActivity.class));
+            }
+        });
     }
 }
