d75926fe4a3f428ac232d3320bea9943a2c7b7f9
DaviisLV <abols.davis@gmail.com>
2018-11-10 15:25:04 +0200

    first commit

---
+++ app/src/main/java/com/abols/davis/vrvt_rest_ned_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.abols.davis.vrvt_rest_ned_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
